730d431d41327bb52e973be040c724a7245263a3
Michele Finotto <m@finotto.org>
2018-03-08 19:52:42 +0000

    Initial import

---
+++ protector.go
@@ -0,0 +1,24 @@
+package protector
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+var blockedMethods = []string{"POST", "PUT", "PATCH", "DELETE"}
+
+func ProtectorMiddleware(isReadOnly func() bool) func(echo.HandlerFunc) echo.HandlerFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if isReadOnly() {
+				for _, s := range blockedMethods {
+					if s == c.Request().Method {
+						return echo.NewHTTPError(http.StatusServiceUnavailable, "Service is currently in read-only mode")
+					}
+				}
+			}
+			return next(c)
+		}
+	}
+}
